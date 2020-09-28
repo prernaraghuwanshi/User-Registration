73398e58e3df6f27e21ee2abb9ea3b1eb8a83e99
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-28 16:33:00 +0530

    [Prerna]ADDED: Last Name Validation

--- src/com/bridgelabz/userreg/Regex.java
+++ src/com/bridgelabz/userreg/Regex.java
@@ -7,10 +7,14 @@ import java.util.regex.Pattern;
 
 public class Regex {
 	private static final Pattern FNAME_PATTERN = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+	private static final Pattern LNAME_PATTERN = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
 	private static Scanner sc;
     
 	private boolean checkFirstName(String s) {
         return FNAME_PATTERN.matcher(s).matches();
+    }
+	private boolean checkLastName(String s) {
+        return LNAME_PATTERN.matcher(s).matches();
     }
 	public static void main(String args[]) {
 		System.out.println("Welcome to User Registration!");
@@ -24,6 +28,14 @@ public class Regex {
 			System.out.println("First Name is VALID!");
 		else
 			System.out.println("ERROR! First Name is INVALID!");
+		
+		System.out.println("Enter Last Name: ");
+		String lastName = sc.nextLine();
+		boolean lnameValid = r.checkLastName(lastName);
+		if(lnameValid)
+			System.out.println("Last Name is VALID!");
+		else
+			System.out.println("ERROR! Last Name is INVALID!");
 	
 	}
 
