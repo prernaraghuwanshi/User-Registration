7ed8b23bfd8509ecae0dc72c9a81c8bd36689c0c
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-28 16:51:04 +0530

    [Prerna]ADDED: Mobile number validation

--- src/com/bridgelabz/userreg/Regex.java
+++ src/com/bridgelabz/userreg/Regex.java
@@ -9,6 +9,7 @@ public class Regex {
 	private static final Pattern FNAME_PATTERN = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
 	private static final Pattern LNAME_PATTERN = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
 	private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-z0-9]+[_+-.]{0,1}[a-z0-9]+@[a-z0-9]+.[a-z]{2,}(.[a-z]{2}){0,1}$");
+	private static final Pattern MOBILE_PATTERN = Pattern.compile("^[0-9]{2} [0-9]{10}$");
 	private static Scanner sc;
     
 	private boolean checkFirstName(String s) {
@@ -20,6 +21,9 @@ public class Regex {
 	private boolean checkEmail(String s) {
         return EMAIL_PATTERN.matcher(s).matches();
     }
+	private boolean checkMobile(String s) {
+		return MOBILE_PATTERN.matcher(s).matches();
+	}
 	public static void main(String args[]) {
 		System.out.println("Welcome to User Registration!");
 		Regex r = new Regex();
@@ -76,6 +80,22 @@ public class Regex {
 				flag = 1;
 			}
 				
+		}
+		while(flag==0)
+		{
+			System.out.println("Enter Mobile No.: ");
+			String mobile = sc.nextLine();
+			boolean mobileValid = r.checkMobile(mobile);
+			if(mobileValid)
+			{
+				System.out.println("Mobile No. is VALID!");
+				flag = 1;
+			}
+			else
+			{
+				System.out.println("ERROR! Mobile No. is INVALID!");
+				flag = 0;
+			}
 		}
 	
 	}
