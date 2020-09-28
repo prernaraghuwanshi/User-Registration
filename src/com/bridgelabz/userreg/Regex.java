7a4eefd006280d26b128bba679ecffb2f2cf5252
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-28 16:42:28 +0530

    [Prerna]ADDED: Email validation

--- src/com/bridgelabz/userreg/Regex.java
+++ src/com/bridgelabz/userreg/Regex.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 public class Regex {
 	private static final Pattern FNAME_PATTERN = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
 	private static final Pattern LNAME_PATTERN = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+	private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-z0-9]+[_+-.]{0,1}[a-z0-9]+@[a-z0-9]+.[a-z]{2,}(.[a-z]{2}){0,1}$");
 	private static Scanner sc;
     
 	private boolean checkFirstName(String s) {
@@ -16,26 +17,66 @@ public class Regex {
 	private boolean checkLastName(String s) {
         return LNAME_PATTERN.matcher(s).matches();
     }
+	private boolean checkEmail(String s) {
+        return EMAIL_PATTERN.matcher(s).matches();
+    }
 	public static void main(String args[]) {
 		System.out.println("Welcome to User Registration!");
 		Regex r = new Regex();
 		sc = new Scanner(System.in);
-		
-		System.out.println("Enter First Name: ");
-		String firstName = sc.nextLine();
-		boolean fnameValid = r.checkFirstName(firstName);
-		if(fnameValid)
-			System.out.println("First Name is VALID!");
-		else
-			System.out.println("ERROR! First Name is INVALID!");
-		
-		System.out.println("Enter Last Name: ");
-		String lastName = sc.nextLine();
-		boolean lnameValid = r.checkLastName(lastName);
-		if(lnameValid)
-			System.out.println("Last Name is VALID!");
-		else
-			System.out.println("ERROR! Last Name is INVALID!");
+		int flag = 1;
+		while(flag==1)
+		{
+			System.out.println("Enter First Name: ");
+			String firstName = sc.nextLine();
+			boolean fnameValid = r.checkFirstName(firstName);
+			if(fnameValid)
+			{
+				System.out.println("First Name is VALID!");
+				flag = 0;
+			}
+				
+			else
+			{
+				System.out.println("ERROR! First Name is INVALID!");
+				flag = 1;
+			}
+				
+		}
+		while(flag==0)
+		{
+			System.out.println("Enter Last Name: ");
+			String lastName = sc.nextLine();
+			boolean lnameValid = r.checkLastName(lastName);
+			if(lnameValid)
+			{
+				System.out.println("Last Name is VALID!");
+				flag = 1;
+			}
+			else
+			{
+				System.out.println("ERROR! Last Name is INVALID!");
+				flag = 0;
+			}
+		}
+		while(flag==1)
+		{
+			System.out.println("Enter Email: ");
+			String email = sc.nextLine();
+			boolean emailValid = r.checkEmail(email);
+			if(emailValid)
+			{
+				System.out.println("Email is VALID!");
+				flag = 0;
+			}
+				
+			else
+			{
+				System.out.println("ERROR! Email is INVALID!");
+				flag = 1;
+			}
+				
+		}
 	
 	}
 
